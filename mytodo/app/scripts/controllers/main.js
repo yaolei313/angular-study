c4559cb32b636e5b0d9ce49ab4929e154c5ec153
yaolei313 <yaolei313@gmail.com>
2015-04-29 00:09:26 +0800

    http://yeoman.io/codelab/write-app.html

---
+++ mytodo/app/scripts/controllers/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.todos = [
+      'Item 1',
+      'Item 2',
+      'Item 3',
+      'Item 4'
+    ];
+    $scope.addTodo = function(){
+    	$scope.todos.push($scope.todo);
+    	$scope.todo='';
+    };
+  });
